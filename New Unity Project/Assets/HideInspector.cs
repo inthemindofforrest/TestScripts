3ecb2a695ad31b41a9c673c1af9f84e30a892367
inthemindofforrest <mccarthy_forrest@outlook.com>
2019-04-10 22:18:47 -0700

    Test

---
+++ New Unity Project/Assets/HideInspector.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(TestScript))]
+public class HideInspector : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        var myScript = target as TestScript;
+
+        using (new EditorGUI.DisabledScope(myScript.Warrior))
+        {
+            myScript.Mage = EditorGUILayout.Toggle("Mage", myScript.Mage);
+
+            using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(myScript.Mage)))
+            {
+                if (group.visible == true)
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.PrefixLabel("Player Color");
+                    myScript.PlayerColor = EditorGUILayout.ColorField(myScript.PlayerColor);
+                    EditorGUILayout.PrefixLabel("Player Name");
+                    myScript.Name = EditorGUILayout.TextField(myScript.Name);
+                    //-----------------------------------------------
+                    EditorGUILayout.PrefixLabel("Hide Info");
+                    myScript.DisableTest = EditorGUILayout.Toggle(myScript.DisableTest);
+                    //-----------------------------------------------
+                    using (new EditorGUI.DisabledScope(myScript.DisableTest))
+                    {
+                        myScript.Age = EditorGUILayout.IntField("Age", myScript.Age);
+                        myScript.Weight = EditorGUILayout.FloatField("Weight", myScript.Weight);
+                        myScript.Height = EditorGUILayout.FloatField("Height", myScript.Height);
+                    }
+                    EditorGUI.indentLevel--;
+                }
+            }
+        }
+
+        using (new EditorGUI.DisabledScope(myScript.Mage))
+        {
+            myScript.Warrior = EditorGUILayout.Toggle("Warrior", myScript.Warrior);
+
+            using (var group = new EditorGUILayout.FadeGroupScope(Convert.ToSingle(myScript.Warrior)))
+            {
+                if (group.visible == true)
+                {
+                    EditorGUI.indentLevel++;
+                    EditorGUILayout.PrefixLabel("Player Color");
+                    myScript.PlayerColor = EditorGUILayout.ColorField(myScript.PlayerColor);
+                    EditorGUILayout.PrefixLabel("Player Name");
+                    myScript.Name = EditorGUILayout.TextField(myScript.Name);
+                    //-----------------------------------------------
+                    EditorGUILayout.PrefixLabel("Enable Default Stats");
+                    myScript.DisableTest = EditorGUILayout.Toggle(myScript.DisableTest);
+                    //-----------------------------------------------
+                    using (new EditorGUI.DisabledScope(myScript.DisableTest))
+                    {
+                        myScript.WarriorStats = EditorGUILayout.Vector3Field("Stats", myScript.WarriorStats);
+                    }
+                    EditorGUI.indentLevel--;
+                }
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class TestScript : MonoBehaviour
+{
+    public bool Mage = false;
+
+    public string Name;
+    public Color PlayerColor;
+    public int Age;
+    public float Weight;
+    public float Height;
+
+    public bool DisableTest;
+
+    public Vector3 WarriorStats;
+
+    public bool Warrior = false;
+}
